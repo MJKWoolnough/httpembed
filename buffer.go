98b0133da3201dfbe962d95166f34f583db1b9b9
Michael Woolnough <michael.woolnough@gmail.com>
2022-07-09 13:23:27 +0100

    some readers will return EOF when completed so we need to check for this

--- buffer.go
+++ buffer.go
@@ -4,6 +4,7 @@ package httpembed
 import (
 	"bytes"
 	"compress/gzip"
+	"io"
 	"net/http"
 	"time"
 
@@ -37,7 +38,7 @@ func HandleBuffer(compressed []byte, size int, lastMod time.Time) http.Handler {
 		panic(err)
 	}
 	uncompressed := make([]byte, size)
-	if _, err := g.Read(uncompressed); err != nil {
+	if n, err := g.Read(uncompressed); err != io.EOF && n != size {
 		panic(err)
 	}
 	return &buffers{
