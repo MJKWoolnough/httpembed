f3024397dc62ba714e3fa1071a9ac053513e8f10
Michael Woolnough <michael.woolnough@gmail.com>
2023-06-05 18:19:11 +0100

    added tests for DecompressFS

--- fs_test.go
+++ fs_test.go
@@ -3,6 +3,8 @@ package httpembed
 import (
 	"errors"
 	"io"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 )
@@ -167,4 +169,100 @@ func TestFiles(t *testing.T) {
 	if !errors.Is(err, ErrNotFound) {
 		t.Errorf("test 30: expecting ErrNotFound, got %s", err)
 	}
+}
+
+func TestDecompressFS(t *testing.T) {
+	c := new(compressor)
+
+	const (
+		valA = "Hello, World!"
+		valB = "12345ABC"
+	)
+	dir := t.TempDir()
+
+	bufA, err := c.Compress(valA)
+	if err != nil {
+		t.Errorf("test 1: unexpected error: %s", err)
+		return
+	}
+
+	if err = writeFile(filepath.Join(dir, "a.txt.gz"), bufA); err != nil {
+		t.Errorf("test 3: unexpected error: %s", err)
+		return
+	}
+
+	bufB, err := c.Compress(valB)
+	if err != nil {
+		t.Errorf("test 2: unexpected error: %s", err)
+		return
+	}
+
+	if err = os.Mkdir(filepath.Join(dir, "dir"), os.ModePerm); err != nil {
+		t.Errorf("test 4: unexpected error: %s", err)
+		return
+	}
+
+	if err = writeFile(filepath.Join(dir, "dir", "b.zip.gz"), bufB); err != nil {
+		t.Errorf("test 5: unexpected error: %s", err)
+		return
+	}
+
+	dd, err := DecompressFS(os.DirFS(dir))
+	if err != nil {
+		t.Errorf("test 6: unexpected error: %s", err)
+		return
+	}
+
+	f, err := dd.Open("a.txt")
+	if err != nil {
+		t.Errorf("test 7: unexpected error: %s", err)
+		return
+	}
+
+	buf, err := io.ReadAll(f)
+	if err != nil {
+		t.Errorf("test 8: unexpected error: %s", err)
+		return
+	}
+
+	if string(buf) != valA {
+		t.Errorf("test 9: expected to read %s, read %s", valA, buf)
+		return
+	}
+
+	f, err = dd.Open("dir/b.zip")
+	if err != nil {
+		t.Errorf("test 10: unexpected error: %s", err)
+		return
+	}
+
+	buf, err = io.ReadAll(f)
+	if err != nil {
+		t.Errorf("test 11: unexpected error: %s", err)
+		return
+	}
+
+	if string(buf) != valB {
+		t.Errorf("test 12: expected to read %s, read %s", valB, buf)
+		return
+	}
+}
+
+func writeFile(path string, data []byte) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
