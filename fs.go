5c35b4b20254463d51cddf136e14a39a15e485a3
Michael Woolnough <michael.woolnough@gmail.com>
2025-09-25 06:51:23 +0100

    split DecompressFS for easier reading

--- fs.go
+++ fs.go
@@ -48,42 +48,43 @@ func DecompressFS(files fs.FS) (fs.FS, error) {
 		dir := filepath.Dir(path)
 
 		if dir != "." && dir != "" && dir != "/" {
-			err = mfs.MkdirAll(dir, fs.ModePerm)
-			if err != nil {
+			if err = mfs.MkdirAll(dir, fs.ModePerm); err != nil {
 				return err
 			}
 		}
 
-		gpath := strings.TrimSuffix(path, ".gz")
-
-		gf, err := mfs.Create(gpath)
-		if err != nil {
-			return err
-		}
-
-		_, err = io.Copy(gf, g)
-		if err != nil {
-			return err
-		}
-
-		if err = gf.Close(); err != nil {
-			return err
-		}
-
-		mtime := info.ModTime()
-
-		if err = mfs.Chtimes(gpath, mtime, mtime); err != nil {
-			return err
-		}
-
-		if err = mfs.Chmod(gpath, info.Mode()); err != nil {
-			return err
-		}
-
-		return nil
+		return writeFileToFS(mfs, strings.TrimSuffix(path, ".gz"), g, info)
 	}); err != nil {
 		return nil, err
 	}
 
 	return mfs.Seal(), nil
+}
+
+func writeFileToFS(mfs *memfs.FS, gpath string, g io.Reader, info fs.FileInfo) error {
+	gf, err := mfs.Create(gpath)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(gf, g)
+	if err != nil {
+		return err
+	}
+
+	if err = gf.Close(); err != nil {
+		return err
+	}
+
+	mtime := info.ModTime()
+
+	if err = mfs.Chtimes(gpath, mtime, mtime); err != nil {
+		return err
+	}
+
+	if err = mfs.Chmod(gpath, info.Mode()); err != nil {
+		return err
+	}
+
+	return nil
 }
